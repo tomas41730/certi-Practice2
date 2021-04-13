6bfd83cbe847b9e309f4394e8fabaaae156009a6
Tomas Carvajal <tomyandrei@hotmail.com>
2021-04-13 18:38:36 -0400

    Edited classes Student and StudentsController

---
+++ Practice2/Practice2/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private List<Student> students;
+        
+
+        public StudentsController(ILogger<StudentsController> logger)
+        {
+        }
+
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+        [HttpPost]
+        public Student CreateStudent([FromBody]string studentName)
+        {
+            return new Student() 
+            { 
+                Name = studentName 
+            };
+        }
+        [HttpPut]
+        public Student UpdateStudent([FromBody]Student student)
+        {
+            student.Name = "updated";
+            return student;
+        }
+        [HttpDelete]
+        public Student DeleteStudent([FromBody]Student student)
+        {
+            student.Name = "deleted";
+            return student;
+        }
+        public void populateList()
+        {
+            students = new List<Student>();
+            students.Add(new Student() { Name = "Susy" });
+            students.Add(new Student() { Name = "Carlos" });
+            students.Add(new Student() { Name = "Alejandro" });
+            students.Add(new Student() { Name = "Diana" });
+            students.Add(new Student() { Name = "Max" });
+            students.Add(new Student() { Name = "Camila" });
+        }
+    }
+}
---
+++ Practice2/Practice2/Student.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Practice2
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
